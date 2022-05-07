bee327ffba50b95339790b81e259feb770f4a1d3
vjozic91 <vjozic@fesb.hr>
2022-05-07 19:07:07 +0200

    feat(to-do-data): enter to do data in form

--- ToDoAngularAspNetCore/ClientApp/src/app/app.module.ts
+++ ToDoAngularAspNetCore/ClientApp/src/app/app.module.ts
@@ -3,12 +3,19 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatNativeDateModule, MatRippleModule, MAT_DATE_LOCALE } from '@angular/material/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { ToDoComponent } from './to-do/to-do.component';
+
 
 @NgModule({
   declarations: [
@@ -16,19 +23,27 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
     NavMenuComponent,
     HomeComponent,
     CounterComponent,
-    FetchDataComponent
+    FetchDataComponent,
+    ToDoComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
+    BrowserAnimationsModule,
+    MatNativeDateModule,
+    MatRippleModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
       { path: 'fetch-data', component: FetchDataComponent },
+      { path: 'to-do', component: ToDoComponent }
     ])
   ],
-  providers: [],
+  providers: [{ provide: MAT_DATE_LOCALE, useValue: 'hr' }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
